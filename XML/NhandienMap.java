16e4aa2065e58ea6c67226630dac492357d1f9fd
Nguyễn Tín <144228743+Cimelody210@users.noreply.github.com>
2024-07-27 21:36:30 +0700

    yy

---
+++ XML/NhandienMap.java
@@ -0,0 +1,7 @@
+public class report extends AppCompatActivity{
+    protected void onCreate().addConvertFactory(GsonConvertorFactor factor).add(){
+        private static void LatLng previous = null;
+        public void DrawRoutes(String resResspone){}
+        public static void getColor(int routes_color){}
+    }
+}
